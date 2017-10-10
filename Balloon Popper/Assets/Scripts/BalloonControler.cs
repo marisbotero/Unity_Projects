e34c02c332ba88af72ffc275d166df507c65e2bc
marisbotero <maris2143@gmail.com>
2017-10-10 13:53:01 -0500

    Ballon, simple scrip

---
+++ Balloon Popper/Assets/Scripts/BalloonControler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonControler : MonoBehaviour {
+
+	// how much it grows each time
+	public float scaleFactor = 1.2f;
+
+
+	//maxium scale
+	public float maxScale = 3;
+
+	// Use this for initialization
+	
+	//Method that will detete mouse press
+
+	void Star(){
+
+		if (scaleFactor <=1){
+			print("It's too small");
+		}
+
+	}
+
+	void OnMouseDown(){
+		//increase the scale 
+		//transform.localScale = transform.localScale * scaleFactor;
+		transform.localScale *= scaleFactor;	
+
+	    //increase this value
+
+		//check if we've passed the maxS
+		if (transform.localScale.x >= maxScale){
+			//if so, we want to destroy the ballon
+			Destroy(gameObject);
+		}
+
+
+	}
+	
+}
