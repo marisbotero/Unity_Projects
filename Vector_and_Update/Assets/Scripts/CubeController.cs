8ff393b0606aa7f83fd5b5fd551e4aa634267083
marisbotero <maris2143@gmail.com>
2017-10-10 14:57:28 -0500

    Working with vector in Unity

---
+++ Vector_and_Update/Assets/Scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+
+	public float speed = 0.1f;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += speed * Vector3.up;
+	
+
+	// we want to our cube to move up and down between y=-10 and y=10
+		if (transform.position.y >=10 && speed > 0){
+			speed *= -1;
+
+		}
+
+		else if(transform.position.y <=-10 && speed <0){
+			speed*=-1;
+		}
+
+	}
+}
